1bb740a25072879a6461793f446a948e85ca5304
Devyn Simeoni <devynsimeoni@gmail.com>
2024-10-18 15:36:56 +0100

    Completed orignal model and almost done with the wolf limit

--- WeclawModelAttempt.R
+++ WeclawModelAttempt.R
@@ -3,36 +3,36 @@ graphics.off()
 quartz()
 
 library(ggplot2)
-edit
+
 
 #############
 ##Model skeleton
 #############
 
+#ORIGINAL MODEL
+
 ##set up parameter values
-sP <- ...
-Kp <- ...
-lP <- ...
-hp <- ...
-sL <- ...
-Kl <- ...
-lL <- ...
-hl <- ...
-sH <- ...
-Kh <- ...
-lH <- ...
-hh <- ...
-fC <- ...
-eC <- ...
-mC <- ...
-fM <- ...
-eM <- ...
-mM <- ...
-mW <- ...
-b <- ...
-g <- ...
-dC <- ...
-dM <- ...
+sP <- 0.5
+Kp <- 240
+lP <- 2.68275
+hp <- 300
+sL <- 0.07
+Kl <- 870
+lL <- 2.68275
+hl <- 400
+sH <- 0.5
+Kh <- 970
+lH <- 3.1682
+hH <- 1000
+fC <- 1
+eC <- 1.85
+mC <- 0
+fM <- 1.5
+eM <- 0.6
+b <- 0.8
+g <- 0.38
+dC <- 460
+dM <- 46
 
 
 
@@ -40,26 +40,45 @@ nsteps <- 200
 pop.df.0 <- data.frame(time=1:nsteps,P=numeric(nsteps),L=numeric(nsteps),H=numeric(nsteps),C=numeric(nsteps),M=numeric(nsteps),W=numeric(nsteps))
 
 pop.df.0 <- within(pop.df.0,{
-  P[1] <- ...
-  L[1] <- ...
-  H[1] <- ...
-  C[1] <- ...
-  M[1] <- ...
-  W[1] <- ...
+  P[1] <- 240
+  L[1] <- 870
+  H[1] <- 970
+  C[1] <- 7
+  M[1] <- 25
+  W[1] <- 8
 })
 
 
-for(t in 1:(nsteps-1)){
+for(t in 2:(nsteps)){
   pop.df.0 <- within(pop.df.0,{
-    P[t+1] <- max(0,...) ##plants consumed by Caribou
-    L[t+1] <- max(0,...) ##lichen consumed by Caribou
-    H[t+1] <- max(0,...) ##plants consumed by Moose
+    #Primary Producers
+    P.birth <- sP*P[t-1]*(1-P[t-1]/Kp)
+    P.death <- (C[t-1]*P[t-1]*lP)/(hp+P[t-1])
+    P[t] <- max(0,P[t-1]+P.birth-P.death) ##plants consumed by Caribou
     
-    C[t+1] <- max(0,...)
+    L.birth <- sL*L[t-1]*(1-L[t-1]/Kl)
+    L.death <- (C[t-1]*lL*L[t-1])/(hl+L[t-1])
+    L[t] <- max(0,L[t-1]+L.birth-L.death) ##lichen consumed by Caribou
     
-    M[t+1] <- max(0,...)
+    H.birth <- sH*H[t-1]*(1-H[t-1]/Kh)
+    H.death <- lH*H[t-1]*M[t-1]/(hH+H[t-1])
+    H[t] <- max(0,H[t-1] + H.birth - H.death) ##plants consumed by Moose (foraging)
     
-    W[t] <- max(0,...)
+    #Primary Predation
+    C.growth <- C[t-1]*fC*(P[t-1]/(P[t-1]+hp))*(L[t-1]/(L[t-1]+hl))
+    C.death <- C[t-1]*(1-P[t-1]/(P[t-1]+hp))*(1-L[t-1]/(L[t-1]+hl))
+    C.pred <- W[t-1]*eC*C[t-1]/(C[t-1]+dC) 
+    C[t] <- max(0,C[t-1] + C.growth - C.death - C.pred) #Caribou
+    
+    M.growth <- M[t-1]*fM*H[t-1]/(H[t-1]+hH)
+    M.death <- M[t-1]*(1-H[t-1]/(H[t-1]+hH))
+    M.pred <- W[t-1]*eM*M[t-1]/(M[t-1]+dM)
+    M[t] <- max(0,M[t-1] + M.growth - M.death - M.pred) #Moose
+    
+    #Predator
+    W.growth <- (b*(C[t-1]/(dC+C[t-1])) + b*(M[t-1]/(dM+M[t-1])))*W[t-1]
+    W.death <- g*W[t-1]
+    W[t] <- max(0,W[t-1] + W.growth - W.death)
   })
 }
 
@@ -80,5 +99,91 @@ ggplot(data = pop.df.0)+
 
 
 
+#MODEL WITH LIMIT ON WOLF POPULATIONS
+
+##set up parameter values
+sP <- 0.5
+Kp <- 240
+lP <- 2.68275
+hp <- 300
+sL <- 0.07
+Kl <- 870
+lL <- 2.68275
+hl <- 400
+sH <- 0.5
+Kh <- 970
+lH <- 3.1682
+hH <- 1000
+fC <- 1
+eC <- 1.85
+mC <- 0
+fM <- 1.5
+eM <- 0.6
+b <- 0.8
+g <- 0.38
+dC <- 460
+dM <- 46
+
+
+
+nsteps <- 200
+pop.df.2 <- data.frame(time=1:nsteps,P=numeric(nsteps),L=numeric(nsteps),H=numeric(nsteps),C=numeric(nsteps),M=numeric(nsteps),W=numeric(nsteps))
+
+pop.df.2 <- within(pop.df.2,{
+  P[1] <- 240
+  L[1] <- 870
+  H[1] <- 970
+  C[1] <- 7
+  M[1] <- 25
+  W[1] <- 8
+})
+
+
+for(t in 2:(nsteps)){
+  pop.df.0 <- within(pop.df.0,{
+    #Primary Producers
+    P.birth <- sP*P[t-1]*(1-P[t-1]/Kp)
+    P.death <- (C[t-1]*P[t-1]*lP)/(hp+P[t-1])
+    P[t] <- max(0,P[t-1]+P.birth-P.death) ##plants consumed by Caribou
+    
+    L.birth <- sL*L[t-1]*(1-L[t-1]/Kl)
+    L.death <- (C[t-1]*lL*L[t-1])/(hl+L[t-1])
+    L[t] <- max(0,L[t-1]+L.birth-L.death) ##lichen consumed by Caribou
+    
+    H.birth <- sH*H[t-1]*(1-H[t-1]/Kh)
+    H.death <- lH*H[t-1]*M[t-1]/(hH+H[t-1])
+    H[t] <- max(0,H[t-1] + H.birth - H.death) ##plants consumed by Moose (foraging)
+    
+    #Primary Predation
+    C.growth <- C[t-1]*fC*(P[t-1]/(P[t-1]+hp))*(L[t-1]/(L[t-1]+hl))
+    C.death <- C[t-1]*(1-P[t-1]/(P[t-1]+hp))*(1-L[t-1]/(L[t-1]+hl))
+    C.pred <- W[t-1]*eC*C[t-1]/(C[t-1]+dC) 
+    C[t] <- max(0,C[t-1] + C.growth - C.death - C.pred) #Caribou
+    
+    M.growth <- M[t-1]*fM*H[t-1]/(H[t-1]+hH)
+    M.death <- M[t-1]*(1-H[t-1]/(H[t-1]+hH))
+    M.pred <- W[t-1]*eM*M[t-1]/(M[t-1]+dM)
+    M[t] <- max(0,M[t-1] + M.growth - M.death - M.pred) #Moose
+    
+    #Predator
+    W.growth <- (b*(C[t-1]/(dC+C[t-1])) + b*(M[t-1]/(dM+M[t-1])))*W[t-1]
+    W.death <- g*W[t-1]
+    W[t] <- min(max(0,W[t-1] + W.growth - W.death), 10)
+  })
+}
+
+
+colors <- c("Plants"="brown","Lichen"="orange","Shrubs"="green","Caribou"="purple","Moose"="blue","Wolf"="red")
+ggplot(data = pop.df.0)+
+  geom_line(mapping=aes(x=time,y=P,color="Plants")) +
+  geom_line(mapping=aes(x=time,y=L,color="Lichen")) +
+  geom_line(mapping=aes(x=time,y=H,color="Shrubs")) +
+  geom_line(mapping=aes(x=time,y=C,color="Caribou")) +
+  geom_line(mapping=aes(x=time,y=M,color="Moose")) +
+  geom_line(mapping=aes(x=time,y=W,color="Wolf")) +
+  geom_hline(yintercept=0,color="darkgrey") +
+  geom_vline(xintercept=0,color="darkgrey") +
+  labs(x = "Time", y = "Densities", color="Legend")+
+  scale_color_manual(values = colors)
 
 
